ab42585cbe524a90f1de2911daf8fe4ffc49345f
abhinav3254 <abhinav3254@gmail.com>
2022-10-19 11:57:53 +0530

    Codeforces Round #827 (Div. 4)

---
+++ Codeforces Round #827 (Div. 4)/Two.java
@@ -0,0 +1,34 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		        String[] strNums;
+		
+		while (t-->0) {
+			
+			            strNums = br.readLine().split(" ");
+			
+			int a = Integer.parseInt(strNums[0]);
+			int b = Integer.parseInt(strNums[1]);
+			int c = Integer.parseInt(strNums[2]);
+			
+			a = a-1;
+			
+			b = b-c;
+			if (b<0)
+			b = b*-1;
+			b = b+(c-1);
+			
+			if (a<b) {
+				System.out.println("1");
+			} else if (a>b) {
+				System.out.println("2");
+			} else if (a==b) {
+				System.out.println("3");
+			}
+		}
+		
+	}
+}
